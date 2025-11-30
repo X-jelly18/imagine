feac543255728a6a6054a667e491416c37c5f13e
X-jelly18 <elitegamer128k@gmail.com>
2025-12-01 00:09:56 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
+  const backend = "https://gsa.ayanakojivps.shop";
+  const path = "/proxy/xhttp";
+
+  const targetURL = backend + path;
+
+  const response = await fetch(targetURL, {
+    method: req.method,
+    headers: req.headers,
+    body: req.method === "GET" ? undefined : req.body
+  });
+
+  const buffer = await response.arrayBuffer();
+
+  res.status(response.status);
+  response.headers.forEach((v, k) => res.setHeader(k, v));
+  res.send(Buffer.from(buffer));
+}
 
