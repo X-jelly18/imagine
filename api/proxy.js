552b084701870d587b3b57f09b19387234c4f519
X-jelly18 <elitegamer128k@gmail.com>
2025-12-07 13:44:31 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 module.exports = async (req, res) => {
-const backendHost = 'holy-surf-0636.fuuuuyyy7.workers.dev';
+const backendHost = 'gsa.ayanakojivps.shop';
 const backendPath = req.url;
 
 const options = {
