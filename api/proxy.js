8fee221b25301264e99e0646897ce06ab0878781
X-jelly18 <elitegamer128k@gmail.com>
2025-12-20 15:14:04 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,30 +1,35 @@
 const https = require('https');
 
 module.exports = async (req, res) => {
-const backendHost = 'gsa.ayanakojivps.shop';
-const backendPath = req.url;
+  const backendHost = 'gsa.ayanakojivps.shop';
+  const backendPath = req.url;
 
-const options = {
-hostname: backendHost,
-port: 443,
-path: backendPath,
-method: req.method,
-headers: { ...req.headers, host: backendHost },
-};
+  const options = {
+    hostname: backendHost,
+    port: 443,
+    path: backendPath,
+    method: req.method,
+    headers: { ...req.headers, host: backendHost },
+  };
 
-const backendReq = https.request(options, backendRes => {
-res.writeHead(backendRes.statusCode, backendRes.headers);
-backendRes.pipe(res, { end: true });
-});
+  const backendReq = https.request(options, backendRes => {
+    // Send headers immediately
+    res.writeHead(backendRes.statusCode, backendRes.headers);
 
-backendReq.on('error', err => {
-console.error('Backend request error:', err);
-res.status(502).send('Bad Gateway');
-});
+    // Stream response directly to client with small buffer
+    backendRes.pipe(res, { end: true, highWaterMark: 16 * 1024 }); // 16 KB chunks
+  });
 
-if (req.method !== 'GET' && req.method !== 'HEAD') {
-req.pipe(backendReq, { end: true });
-} else {
-backendReq.end();
-}
+  backendReq.on('error', err => {
+    console.error('Backend request error:', err);
+    if (!res.headersSent) res.status(502).send('Bad Gateway');
+    else res.end();
+  });
+
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    // Stream request with small buffer to backend
+    req.pipe(backendReq, { end: true, highWaterMark: 16 * 1024 }); // 16 KB chunks
+  } else {
+    backendReq.end();
+  }
 };
