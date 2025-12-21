493a40c603a17617353155935c4227143b7232d2
X-jelly18 <elitegamer128k@gmail.com>
2025-12-21 13:13:52 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 module.exports = async (req, res) => {
-  const backendHost = 'gsa.ayanakojivps.shop';
+  const backendHost = 'allin1.ayanakojixxx.shop';
   const backendPath = req.url;
 
   const options = {
