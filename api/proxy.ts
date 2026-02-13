05575856518d713c85264d56bcb4931468cd7ba9
X-jelly18 <elitegamer128k@gmail.com>
2026-02-13 03:36:51 +0200

    Update proxy.ts

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "https://shayibuzi.kingbbxvggshop.shop" +
+    "https://za.ayanakojivps.shop" +
     url.pathname +
     url.search;
 
